aae40bf624a641fe038e5b6137c0b4a7c9d8afcd
colesluggett <35312647+colesluggett@users.noreply.github.com>
2019-02-15 10:12:45 -0700

    We are trying

--- ESOFWebsite/api/User/userTest.php
+++ ESOFWebsite/api/User/userTest.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
-use PHPUnit\Framework\TestCase;
-final class userTest extends TestCase
+final class userTest extends PHPUnit_Framework_TestCase
 {
     public function testCanBeCreatedFromValidEmailAddress(): void
     {
@@ -21,5 +20,82 @@ final class userTest extends TestCase
             'user',
             signup::fromString('user')
         );
+    }
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     */
+    public function testConsumer()
+    {
+        $this->assertEquals(
+            ['first', 'second'],
+            func_get_args()
+        );
+    }
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertEquals($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            'adding zeros'  => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one'  => [1, 1, 3]
+        ];
     }
 }
