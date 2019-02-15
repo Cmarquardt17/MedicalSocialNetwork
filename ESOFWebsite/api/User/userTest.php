ec607a0927914a4efa152a73472012b457c365c9
Cmarquardt17 <christianmarquardt14@hotmail.com>
2019-02-15 10:14:24 -0700

    Creating a duplicate for further use and testing

---
+++ ESOFWebsite/api/User/userTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class userTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            signup::class,
+            signup::fromString('user')
+        );
+    }
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        signup::fromString('invalid');
+    }
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user',
+            signup::fromString('user')
+        );
+    }
+}
