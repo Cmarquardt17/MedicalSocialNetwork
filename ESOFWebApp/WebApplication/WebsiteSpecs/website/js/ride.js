1002341906ddd9779b47a820f8a537046c149036
colesluggett <35312647+colesluggett@users.noreply.github.com>
2019-02-28 20:31:25 -0700

    ride.js

--- ESOFWebApp/WebApplication/WebsiteSpecs/website/js/ride.js
+++ ESOFWebApp/WebApplication/WebsiteSpecs/website/js/ride.js
@@ -81,25 +81,6 @@ WildRydes.map = WildRydes.map || {};
         requestdoctor(pickupLocation);
     }
 
-    function animateArrival(callback) {
-        var dest = WildRydes.map.selectedPoint;
-        var origin = {};
-
-        if (dest.latitude > WildRydes.map.center.latitude) {
-            origin.latitude = WildRydes.map.extent.minLat;
-        } else {
-            origin.latitude = WildRydes.map.extent.maxLat;
-        }
-
-        if (dest.longitude > WildRydes.map.center.longitude) {
-            origin.longitude = WildRydes.map.extent.minLng;
-        } else {
-            origin.longitude = WildRydes.map.extent.maxLng;
-        }
-
-        WildRydes.map.animate(origin, dest, callback);
-    }
-
     function displayUpdate(text) {
         $('#updates').append($('<li>' + text + '</li>'));
     }
