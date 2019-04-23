aed9796cbe63ea31595e6ee9b7426f43474c1a73
Cmarquardt17 <christianmarquardt14@hotmail.com>
2019-04-22 19:10:27 -0600

    The latest test cases from Christian

--- Project/test_login.py
+++ Project/test_login.py
@@ -3,9 +3,10 @@
 import unittest
 import os,sys,inspect
 from project import create_app
+#from project import Blueprint
 
 app = create_app()
-
+#errors = Blueprint('errors', __name__)
 
 class BasicTests(unittest.TestCase):
 
@@ -16,9 +17,9 @@ class BasicTests(unittest.TestCase):
     # executed prior to each test
     def setUp(self):
         app.config['TESTING'] = True
-        app.config['DEBUG'] = False
+        app.config['DEBUG'] = True
         self.app = app.test_client()
-        self.assertEqual(app.debug, False)
+        #self.assertEqual(app.debug, False)
         # Disable sending emails during unit testing
 
     # executed after each test
@@ -28,6 +29,12 @@ class BasicTests(unittest.TestCase):
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_Handler(self):
+        response = self.app.get('/something')
+        response2 = self.app.get('/post/1/update')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response2.status_code, 302)
 
     def test_login_page_loads(self):
         response = self.app.get('/login')
@@ -41,6 +48,11 @@ class BasicTests(unittest.TestCase):
         )
         self.assertIn(b'Log In', response.data)
 
+    def test_create_user(self):
+
+        u = User(nickname = 'john', email = 'john@example.com', account_type = "tester")
+        response = self.app.get('/logout')
+        self.assertIn(b'/home', response.data)
 
 if __name__ == "__main__":
     unittest.main()
